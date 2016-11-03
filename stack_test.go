18edd1af8a639efcdfa247160ce5b271805385df
Lukas Dietrich <lukas@lukasdietrich.com>
2016-11-03 15:23:35 +0100

    Add additional stack testcases

--- stack_test.go
+++ stack_test.go
@@ -11,12 +11,16 @@ func TestStack(t *testing.T) {
 	var stack StringStack
 
 	assert.True(stack.Empty(), "default value for a stack should be empty")
+	assert.Equal("", stack.Pop(), "pop value for empty stack should be empty string")
+	assert.Equal("", stack.Peek(), "peek value for empty stack should be empty string")
 
-	stack.Push("test")
+	stack.Push("foo")
 
 	assert.False(stack.Empty(), "after a push the stack should not be empty")
-	assert.Equal("test", stack.Peek(), "peek value should equal the latest push")
-	assert.Equal("test", stack.Pop(), "pop value should equal the latest push")
+	assert.True(stack.Contains("foo"), "stack should contain value after push")
+	assert.False(stack.Contains("bar"), "stack should not contain values that were not pushed")
+	assert.Equal("foo", stack.Peek(), "peek value should equal the latest push")
+	assert.Equal("foo", stack.Pop(), "pop value should equal the latest push")
 	assert.True(stack.Empty(), "after pop the stack should be empty")
 
 	for i := 0; i < 10; i++ {
