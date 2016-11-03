228edbad6fb4cbf188d62c2f43cca86e91077e90
Lukas Dietrich <lukas@lukasdietrich.com>
2016-11-03 15:19:09 +0100

    Use assertion framework for tests

--- stack_test.go
+++ stack_test.go
@@ -2,30 +2,26 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestStack(t *testing.T) {
+	assert := assert.New(t)
 	var stack StringStack
 
-	if !stack.Empty() {
-		t.Error("default value for stack is not empty")
-	}
+	assert.True(stack.Empty(), "default value for a stack should be empty")
 
 	stack.Push("test")
 
-	if stack.Empty() {
-		t.Error("non empty stack marked as empty")
+	assert.False(stack.Empty(), "after a push the stack should not be empty")
+	assert.Equal("test", stack.Peek(), "peek value should equal the latest push")
+	assert.Equal("test", stack.Pop(), "pop value should equal the latest push")
+	assert.True(stack.Empty(), "after pop the stack should be empty")
+
+	for i := 0; i < 10; i++ {
+		stack.Push(" ")
 	}
 
-	if stack.Peek() != "test" {
-		t.Error("incorrect peek")
-	}
-
-	if stack.Pop() != "test" {
-		t.Error("incorrect pop")
-	}
-
-	if !stack.Empty() {
-		t.Error("pop does not empty the stack")
-	}
+	assert.Equal(10, stack.Size(), "stack should contain N elements after N pushes")
 }
